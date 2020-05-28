05cf3ef5ad7d2a612a5b29b35920fd33e03a5ce8
RB28Z2 <kmarasinghe1@gmail.com>
2020-05-27 20:36:49 -0600

    wifi connectivity working

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,10 @@
-//#include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
-//#include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
-// OR #include "SH1106Wire.h"   // legacy: #include "SH1106.h"
-
-// For a connection via I2C using brzo_i2c (must be installed) include:
  #include <brzo_i2c.h>        // Only needed for Arduino 1.6.5 and earlier
  #include <SSD1306BrzoMod.h>
-// OR #include "SH1106Brzo.h"
+ #include <ESP8266WiFi.h>
+ #include <Variables.h>
 
-// For a connection via SPI include:
-// #include <SPI.h>             // Only needed for Arduino 1.6.5 and earlier
-// #include "SSD1306Spi.h"
-// OR #include "SH1106SPi.h"
-
-
+ const char* ssid = WIFI_SSID;
+ const char* pass = WIFI_PASS;
 // Initialize the OLED display using Arduino Wire:
 SSD1306BrzoMod display(0x3c, SDA, SCL, GEOMETRY_64_48);   // ADDRESS, SDA, SCL  -  SDA and SCL usually populate automatically 
 
@@ -22,27 +14,41 @@ void setup() {
   Serial.println();
   Serial.println();
 
-
   // Initialising the UI will init the display too.
   display.init();
 
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+
+  display.drawStringMaxWidth(0, 0, 64, "Connecting to " + String(ssid));
+  display.display();
   
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
+
+  display.clear();
+  display.drawStringMaxWidth(0,0,64, "IP: " + WiFi.localIP().toString());
+  display.display();
+  delay(1500);
+
 }
 
 void drawTextAlignmentDemo() {
+  //display.clear();
     // Text alignment demo
   display.setFont(ArialMT_Plain_10);
 
   // The coordinates define the left starting point of the text
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  
   display.drawString(0, 0, "Left");
 }
 
 
 void loop() {
   drawTextAlignmentDemo();
-  display.display();
+  //display.display();
   delay(100);
 }
